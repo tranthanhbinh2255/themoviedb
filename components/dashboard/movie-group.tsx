e729d6cfcfe08a32f77421d42fa8bd565b312805
Binh Tran <trthanhbinh2255@gmail.com>
2022-04-24 23:21:54 +0700

    add pagination function

--- components/dashboard/movie-group.tsx
+++ components/dashboard/movie-group.tsx
@@ -1,29 +1,42 @@
 import React, { useEffect, useState } from 'react'
 import { ListMovieItemsModel } from '../../models/list-movie-items.model'
-import { getTrending } from '../../utils/movie-api'
+import { getTopRateMovie } from '../../utils/movie-api'
 import AppPagination from '../layout/pagination'
 import MovieListCards from './move-list-cards'
 
 const MovieGroup: React.FC<{}> = ({}) => {
   const [listMovive, setListMovie] = useState<ListMovieItemsModel>(null)
+  const [currentPage, setCurrentPage] = useState(1)
 
   useEffect(async () => {
-    const movieGroup = await getTrending()
+    const movieGroup = await getTopRateMovie(currentPage)
     setListMovie(movieGroup)
-  }, [])
+  }, [currentPage])
 
-  return listMovive && (
-    <div>
-      <h3 className="ui center aligned header">Trending</h3>
-      <div className="ui fluid container">
-        <MovieListCards movieItems={listMovive.results} />
-        <div className="ui grid container">
-          <div className="column">
-            <AppPagination />
+  const onPageChange = (e, { activePage }) => {
+    setCurrentPage(activePage)
+  }
+
+  return (
+    listMovive && (
+      <div>
+        <h3 className="ui center aligned header">TOP RATE MOVIE</h3>
+        <div className="ui fluid container">
+          <MovieListCards movieItems={listMovive.results} />
+          <div className="ui grid container">
+            <div className="column">
+              <AppPagination
+                activePage={currentPage}
+                totalPage={
+                  listMovive.total_pages > 25 ? 25 : listMovive.total_pages
+                }
+                onPageChange={onPageChange}
+              />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    )
   )
 }
 
